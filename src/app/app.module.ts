1ea47b0a33a4602b113af27221747e1853ed4f32
DannyYanacallo1755 <danny9yanacallo@gmail.com>
2024-05-31 16:17:32 -0500

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,21 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+// geolocation and native-geocoder
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { NativeGeocoder, NativeGeocoderResult, NativeGeocoderOptions } from '@ionic-native/native-geocoder/ngx';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    Geolocation,
+    NativeGeocoder,
+    { 
+      provide: RouteReuseStrategy, 
+      useClass: IonicRouteStrategy
+    }
+  ],
   bootstrap: [AppComponent],
 })
+
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import {
+  NativeGeocoder,
+  NativeGeocoderResult,
+  NativeGeocoderOptions,
+} from '@ionic-native/native-geocoder/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
+  latitude: any = 0; //latitude
+  longitude: any = 0; //longitude
+  address: string;
 
-  constructor() {}
+  constructor(
+    private geolocation: Geolocation,
+    private nativeGeocoder: NativeGeocoder
+  ) {}
 
+  // geolocation options
+  options = {
+    timeout: 10000,
+    enableHighAccuracy: true,
+    maximumAge: 3600,
+  };
+
+  // use geolocation to get user's device coordinates
+  getCurrentCoordinates() {
+    this.geolocation
+      .getCurrentPosition()
+      .then((resp) => {
+        console.log(resp);
+        this.latitude = resp.coords.latitude;
+        this.longitude = resp.coords.longitude;
+        this.getAddress(this.latitude, this.longitude);
+      })
+      .catch((error) => {
+        console.log('Error getting location', error);
+      });
+  }
+
+  // geocoder options
+  nativeGeocoderOptions: NativeGeocoderOptions = {
+    useLocale: true,
+    maxResults: 5,
+  };
+
+  // get address using coordinates
+  getAddress(lat: any, long: any) {
+    this.nativeGeocoder
+      .reverseGeocode(lat, long, this.nativeGeocoderOptions)
+      .then((res: NativeGeocoderResult[]) => {
+        this.address = this.pretifyAddress(res[0]);
+      })
+      .catch((error: any) => {
+        alert('Error getting location' + JSON.stringify(error));
+      });
+  }
+
+  // address
+  pretifyAddress(address: any) {
+    let obj = [];
+    let data = '';
+    for (let key in address) {
+      obj.push(address[key]);
+    }
+    obj.reverse();
+    for (let val in obj) {
+      if (obj[val].length) data += obj[val] + ', ';
+    }
+    return address.slice(0, -2);
+  }
 }
