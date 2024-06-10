446fa11393389e67faa5a2e7286ccaa9e5513994
DannyYanacallo1755 <danny9yanacallo@gmail.com>
2024-06-09 21:57:42 -0500

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyD8B0QNrah_Zim6lY7JTL9tRoI2-_O2IOA",
+    authDomain: "appweb-96050.firebaseapp.com",
+    projectId: "appweb-96050",
+    storageBucket: "appweb-96050.appspot.com",
+    messagingSenderId: "86553890383",
+    appId: "1:86553890383:web:45970e7fc9f6f6853ce43a",
+    measurementId: "G-TC2DFNZXTL"
+  }
 };
